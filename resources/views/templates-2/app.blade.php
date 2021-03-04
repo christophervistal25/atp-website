4b5e3eed180d9cf277c21949e656e48585f76df6
CHris <46248327+christophervistal25@users.noreply.github.com>
2021-03-04 21:12:55 +0800

    modify the mobile menu navigation of municipal

--- resources/views/templates-2/app.blade.php
+++ resources/views/templates-2/app.blade.php
@@ -29,7 +29,7 @@
         <!-- BEGIN: Side Menu -->
         <nav class="side-nav">
             <a href="{{  route('admin.dashboard') }}" class="intro-x flex items-center pl-5 pt-4 outline-none">
-                <img alt="Midone Tailwind HTML Admin Template" class="w-6" src="/dist/images/logo.png">
+                <img alt="Logo" class="w-6" src="/dist/images/logo.png">
                 <span class="hidden xl:block text-white text-lg ml-3"> SurSur-<span class="font-medium">ATP</span>
                 </span>
             </a>
@@ -367,118 +367,10 @@
             <!-- BEGIN: Top Bar -->
             <div class="top-bar">
                 <!-- BEGIN: Breadcrumb -->
-                <div class="-intro-x breadcrumb mr-auto hidden sm:flex"> <a href="" class="">Application</a> <i
-                        data-feather="chevron-right" class="breadcrumb__icon"></i> <a href=""
-                        class="breadcrumb--active">Dashboard</a> </div>
-                <!-- END: Breadcrumb -->
-                <!-- BEGIN: Search -->
-                <div class="intro-x relative mr-3 sm:mr-6">
-                    <div class="search hidden sm:block">
-                        <input type="text" class="search__input input placeholder-theme-13" placeholder="Search...">
-                        <i data-feather="search" class="search__icon"></i>
-                    </div>
-                    <a class="notification sm:hidden" href=""> <i data-feather="search" class="notification__icon"></i>
-                    </a>
-                    <div class="search-result">
-                        <div class="search-result__content">
-                            <div class="search-result__content__title">Pages</div>
-                            <div class="mb-5">
-                                <a href="" class="flex items-center">
-                                    <div
-                                        class="w-8 h-8 bg-theme-18 text-theme-9 flex items-center justify-center rounded-full">
-                                        <i class="w-4 h-4" data-feather="inbox"></i> </div>
-                                    <div class="ml-3">Mail Settings</div>
-                                </a>
-                                <a href="" class="flex items-center mt-2">
-                                    <div
-                                        class="w-8 h-8 bg-theme-17 text-theme-11 flex items-center justify-center rounded-full">
-                                        <i class="w-4 h-4" data-feather="users"></i> </div>
-                                    <div class="ml-3">Users & Permissions</div>
-                                </a>
-                                <a href="" class="flex items-center mt-2">
-                                    <div
-                                        class="w-8 h-8 bg-theme-14 text-theme-10 flex items-center justify-center rounded-full">
-                                        <i class="w-4 h-4" data-feather="credit-card"></i> </div>
-                                    <div class="ml-3">Transactions Report</div>
-                                </a>
-                            </div>
-                            <div class="search-result__content__title">Users</div>
-                            <div class="mb-5">
-                                <a href="" class="flex items-center mt-2">
-                                    <div class="w-8 h-8 image-fit">
-                                        <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                            src="/dist/images/profile-3.jpg">
-                                    </div>
-                                    <div class="ml-3">Edward Norton</div>
-                                    <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">
-                                        edwardnorton@left4code.com</div>
-                                </a>
-                                <a href="" class="flex items-center mt-2">
-                                    <div class="w-8 h-8 image-fit">
-                                        <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                            src="/dist/images/profile-3.jpg">
-                                    </div>
-                                    <div class="ml-3">Denzel Washington</div>
-                                    <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">
-                                        denzelwashington@left4code.com</div>
-                                </a>
-                                <a href="" class="flex items-center mt-2">
-                                    <div class="w-8 h-8 image-fit">
-                                        <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                            src="/dist/images/profile-9.jpg">
-                                    </div>
-                                    <div class="ml-3">Morgan Freeman</div>
-                                    <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">
-                                        morganfreeman@left4code.com</div>
-                                </a>
-                                <a href="" class="flex items-center mt-2">
-                                    <div class="w-8 h-8 image-fit">
-                                        <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                            src="/dist/images/profile-8.jpg">
-                                    </div>
-                                    <div class="ml-3">Keira Knightley</div>
-                                    <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">
-                                        keiraknightley@left4code.com</div>
-                                </a>
-                            </div>
-                            <div class="search-result__content__title">Products</div>
-                            <a href="" class="flex items-center mt-2">
-                                <div class="w-8 h-8 image-fit">
-                                    <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                        src="/dist/images/preview-1.jpg">
-                                </div>
-                                <div class="ml-3">Nike Tanjun</div>
-                                <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Sport &amp; Outdoor
-                                </div>
-                            </a>
-                            <a href="" class="flex items-center mt-2">
-                                <div class="w-8 h-8 image-fit">
-                                    <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                        src="/dist/images/preview-9.jpg">
-                                </div>
-                                <div class="ml-3">Nikon Z6</div>
-                                <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Photography</div>
-                            </a>
-                            <a href="" class="flex items-center mt-2">
-                                <div class="w-8 h-8 image-fit">
-                                    <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                        src="/dist/images/preview-12.jpg">
-                                </div>
-                                <div class="ml-3">Sony A7 III</div>
-                                <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Photography</div>
-                            </a>
-                            <a href="" class="flex items-center mt-2">
-                                <div class="w-8 h-8 image-fit">
-                                    <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                        src="/dist/images/preview-13.jpg">
-                                </div>
-                                <div class="ml-3">Samsung Q90 QLED TV</div>
-                                <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Electronic</div>
-                            </a>
-                        </div>
-                    </div>
+                <div class="-intro-x breadcrumb mr-auto hidden sm:flex">
+                    <a href="{{ route('admin.dashboard') }}" class="breadcrumb--active">Dashboard</a>
                 </div>
-                <!-- END: Search -->
+                <!-- END: Breadcrumb -->
                 <!-- BEGIN: Notifications -->
                 <div class="intro-x dropdown relative mr-auto sm:mr-6">
                     <div class="dropdown-toggle notification notification--bullet cursor-pointer"> <i
@@ -499,78 +391,6 @@
                                     <div class="flex items-center">
                                         <a href="javascript:;" class="font-medium truncate mr-5">Edward Norton</a>
                                         <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">03:20 PM</div>
-                                    </div>
-                                    <div class="w-full truncate text-gray-600">Lorem Ipsum is simply dummy text of the
-                                        printing and typesetting industry. Lorem Ipsum has been the industry&#039;s
-                                        standard dummy text ever since the 1500</div>
-                                </div>
-                            </div>
-                            <div class="cursor-pointer relative flex items-center mt-5">
-                                <div class="w-12 h-12 flex-none image-fit mr-1">
-                                    <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                        src="/dist/images/profile-3.jpg">
-                                    <div
-                                        class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white">
-                                    </div>
-                                </div>
-                                <div class="ml-2 overflow-hidden">
-                                    <div class="flex items-center">
-                                        <a href="javascript:;" class="font-medium truncate mr-5">Denzel Washington</a>
-                                        <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">06:05 AM</div>
-                                    </div>
-                                    <div class="w-full truncate text-gray-600">Contrary to popular belief, Lorem Ipsum
-                                        is not simply random text. It has roots in a piece of classical Latin literature
-                                        from 45 BC, making it over 20</div>
-                                </div>
-                            </div>
-                            <div class="cursor-pointer relative flex items-center mt-5">
-                                <div class="w-12 h-12 flex-none image-fit mr-1">
-                                    <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                        src="/dist/images/profile-9.jpg">
-                                    <div
-                                        class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white">
-                                    </div>
-                                </div>
-                                <div class="ml-2 overflow-hidden">
-                                    <div class="flex items-center">
-                                        <a href="javascript:;" class="font-medium truncate mr-5">Morgan Freeman</a>
-                                        <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">01:10 PM</div>
-                                    </div>
-                                    <div class="w-full truncate text-gray-600">There are many variations of passages of
-                                        Lorem Ipsum available, but the majority have suffered alteration in some form,
-                                        by injected humour, or randomi</div>
-                                </div>
-                            </div>
-                            <div class="cursor-pointer relative flex items-center mt-5">
-                                <div class="w-12 h-12 flex-none image-fit mr-1">
-                                    <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                        src="/dist/images/profile-8.jpg">
-                                    <div
-                                        class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white">
-                                    </div>
-                                </div>
-                                <div class="ml-2 overflow-hidden">
-                                    <div class="flex items-center">
-                                        <a href="javascript:;" class="font-medium truncate mr-5">Keira Knightley</a>
-                                        <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">01:10 PM</div>
-                                    </div>
-                                    <div class="w-full truncate text-gray-600">Contrary to popular belief, Lorem Ipsum
-                                        is not simply random text. It has roots in a piece of classical Latin literature
-                                        from 45 BC, making it over 20</div>
-                                </div>
-                            </div>
-                            <div class="cursor-pointer relative flex items-center mt-5">
-                                <div class="w-12 h-12 flex-none image-fit mr-1">
-                                    <img alt="Midone Tailwind HTML Admin Template" class="rounded-full"
-                                        src="/dist/images/profile-10.jpg">
-                                    <div
-                                        class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white">
-                                    </div>
-                                </div>
-                                <div class="ml-2 overflow-hidden">
-                                    <div class="flex items-center">
-                                        <a href="javascript:;" class="font-medium truncate mr-5">Russell Crowe</a>
-                                        <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">01:10 PM</div>
                                     </div>
                                     <div class="w-full truncate text-gray-600">Lorem Ipsum is simply dummy text of the
                                         printing and typesetting industry. Lorem Ipsum has been the industry&#039;s
